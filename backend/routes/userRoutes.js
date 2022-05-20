ae0eea5d946699b8ac299c1bc67b422d3b04fa3b
Joseph Elias <jrelias@outlook.com>
2022-05-20 15:51:08 -0400

    middleware for auth and protected route. JWT functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController');
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController');
+
+const { protect } = require('../middleware/authMiddleware');
 
 // router.post('/', (req, res) => {
 //   res.send('Register Route');
@@ -10,5 +16,7 @@ const { registerUser, loginUser } = require('../controllers/userController');
 router.post('/', registerUser);
 
 router.post('/login', loginUser);
+
+router.get('/me', protect, getMe);
 
 module.exports = router;
