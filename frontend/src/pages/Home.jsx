2797a89535891fcd1be2e730fe2290c238ed16a0
Joseph Elias <jrelias@outlook.com>
2022-05-23 12:55:39 -0400

    beginning home, register, and login pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom"
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa"
+
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <section className="heading">
+        <h1>What do you need help with?</h1>
+        <p>Please choose from an option below</p>
+      </section>
+
+      <Link to='/new-doodle' className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create a new Monster
+      </Link>
+
+      <Link to='/doodles' className="btn btn-block">
+        <FaTicketAlt /> View My Monsters
+      </Link>
+    </>
   )
 }
 
